166cc0488800c30b2403d1f6d3c19a85a63127e4
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-06-03 14:34:50 +0300

    finish sixth ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Second } from "./components/secondsection/second"
 import { Third } from "./components/third section/third"
 import { Fourth } from "./components/fourthsection/fourth"
 import { Fifth } from "./components/fifthsection/fifth"
+import { Sixth } from "./components/sixthsection/sixth"
 function App() {
  
 
@@ -15,6 +16,7 @@ function App() {
         <Third></Third>
         <Fourth></Fourth>
         <Fifth></Fifth>
+        <Sixth></Sixth>
 
     </>
   )
---
+++ src/components/sixthsection/sixth.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export const Sixth:React.FC<{}>=()=>{
+    return(
+         <section className="sixth">
+                  <p>ahmed-rmadan@2025</p>
+                  <p>this project made by me from scratch for training purpose</p>
+                  
+         </section>
+
+
+    )
+}
