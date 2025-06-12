96225bf3b804c4becb16fa3c3c8018bc6db081d5
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-06-12 10:59:57 +0300

    finish locations

--- src/components/fifthsection/fifth.tsx
+++ src/components/fifthsection/fifth.tsx
@@ -1,23 +1,28 @@
 
 import React from "react";
-
+import {Location} from "./location";
 
 export const Fifth:React.FC<{}>=()=>{
+   
     return(
          <section className="fifth">
                 <p>Locations</p>
                 <div className="locations">
                      <div className="start">
                            <p>Cairo</p>
+                           <Location location={[30.045592,31.237981]}></Location>
                      </div>
                      <div className="end">
                            <p>Giza</p>
+                           <Location location={[30.009107,31.200841]}></Location>
                      </div>
                      <div className="start">
                             <p>Alexandria</p>
+                            <Location location={[31.198346567984313,29.894763728265215]}></Location>
                      </div>
                      <div className="end">
                              <p>Tanta</p>
+                             <Location location={[30.78784930823166,31.001840190007467]}></Location>
                      </div>
 
                 </div>
