d307eb6e20211f58b0167cf4823222249b6f3a1f
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-06-12 14:22:14 +0300

    scroll into logic

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,14 +1,19 @@
 import React from "react"
 import { Search } from "../global/srarch"
 import { Cart } from "./cart"
+
 export const Nav:React.FC<{}>=()=>{
+    function getto(name:string){
+        const section=document.querySelector<HTMLDivElement>(`.${name}`)
+    section?.scrollIntoView({behavior:"smooth"})
+    }
     return(
         <nav>
             <div className="name">Food Order</div>
             <div className="nav">  
-              <button>Menu</button>
-              <button> Favorites</button>
-              <button> Locations</button>
+              <button onClick={()=>getto('second')}>Menu</button>
+              <button onClick={()=>getto('fourth')}> Favorites</button>
+              <button onClick={()=>getto('fifth')}> Locations</button>
               <button> Contact</button>
               <Cart></Cart>
               <Search></Search>
